34bcd0a99d354cbeee212cb1f5bacd3f80bdd7d4
HafezCh <hafez.cheraghi1382@gmail.com>
2022-02-17 11:21:01 +0330

    LatestArticle moved to view component

--- DevCard_MVC/Models/Article.cs
+++ DevCard_MVC/Models/Article.cs
@@ -4,6 +4,15 @@
     {
         public long Id { get; set; }
         public string Title { get; set; }
+        public string Image { get; set; }
         public string Description { get; set; }
+
+        public Article(long id, string title, string image, string description)
+        {
+            Id = id;
+            Title = title;
+            Image = image;
+            Description = description;
+        }
     }
 }
---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1,"آموزش Asp.net.core","blog-post-thumb-card-5.jpg","کاملترین پکیج آموزش asp.net"),
+                new Article(2,"آموزش GitHub","blog-post-thumb-card-1.jpg","کاملترین پکیج آموزش Git"),
+                new Article(3,"آموزش Onion Architecture","blog-post-thumb-card-3.jpg","کاملترین پکیج آموزش Onion Architecture"),
+                new Article(4,"آموزش طراحی سایت","blog-post-thumb-card-4.jpg","کاملترین پکیج آموزش طراحی سایت")
+            };
+            return View("_LatestArticles",articles);
+        }
+    }
+}
